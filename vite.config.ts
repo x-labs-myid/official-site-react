0d383d53e74c3fa8d7f3bec98b0d4d1c6da342c7
Kang cahya <cahyadyazin@yahoo.com>
2026-01-11 11:19:31 +0700

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, createLogger } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
+const logger = createLogger();
+const originalWarn = logger.warn;
+
+logger.warn = (msg, options) => {
+  if (msg.includes("Unknown at rule: @property")) return;
+  if (msg.includes("Found 1 warning while optimizing generated CSS")) return;
+  originalWarn(msg, options);
+};
+
 // https://vite.dev/config/
 export default defineConfig({
+  customLogger: logger,
   plugins: [
     react({
       babel: {
@@ -19,6 +29,8 @@ export default defineConfig({
     },
   },
   build: {
+    chunkSizeWarningLimit: 1600,
+    cssTarget: "chrome100",
     rollupOptions: {
       output: {
         manualChunks: {
